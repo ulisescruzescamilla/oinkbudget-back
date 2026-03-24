3b55b9d7fd1babc75629d83adf12d51976909e32
Ulises Escamilla <ulisescruzescamilla@gmail.com>
2026-03-23 23:23:02 -0600

    fix: Resolve error trait class name and account table fix

--- database/migrations/2026_03_15_044024_create_accounts_table.php
+++ database/migrations/2026_03_15_044024_create_accounts_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
     {
         Schema::create('accounts', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
             $table->float('amount', 3);
             $table->enum('type', AccountTypeEnum::values());
             $table->boolean('hidden');
